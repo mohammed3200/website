4a60f59c5beb174ea8dd61d01cde93bdcdc5a274
mohammed3200 <wwyuu799@gmail.com>
2024-11-22 15:06:27 +0200

    fix navbar in mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,20 +3,17 @@
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-
 import { usePathname } from "next/navigation";
 import { useMedia } from "react-use";
-
 import { cn } from "@/lib/utils";
 import useLanguage from "@/hooks/uselanguage";
 import { useTranslations } from "next-intl";
-
 
 export const NavBar = () => {
   const { lang } = useLanguage();
   const pathname = usePathname();
   const t = useTranslations("Navigation");
-  const isDesktopOrTable = useMedia("(min-width: 640px)", true);
+  const isDesktopOrTablet = useMedia("(min-width: 640px)", true);
 
   const navigationItems = [
     { title: t("home"), href: `/${lang}` },
@@ -25,55 +22,41 @@ export const NavBar = () => {
     { title: t("projects"), href: `/${lang}/projects` },
     { title: t("contact"), href: `/${lang}/contact` },
   ];
-  
 
-  const [positionDesktopOrTable, setPositionDesktopOrTable] = useState({
+  const [position, setPosition] = useState({
     left: 0,
-    width: 0,
-    opacity: 0,
-  });
-
-  const [positionMobile, setPositionMobile] = useState({
     top: 0,
     width: 0,
     opacity: 0,
   });
 
+  const handleMouseEnter = (ref: HTMLLIElement | null) => {
+    if (!ref) return;
+    const { width } = ref.getBoundingClientRect();
+    setPosition({
+      left: isDesktopOrTablet ? ref.offsetLeft : 0,
+      top: isDesktopOrTablet ? 0 : ref.offsetTop,
+      width,
+      opacity: 1,
+    });
+  };
+
   return (
     <ul
-      onMouseLeave={() => {
-        if (isDesktopOrTable) {
-          setPositionDesktopOrTable((pv) => ({
-            ...pv,
-            opacity: 0,
-          }));
-        }
-      }}
-      className="relative mx-auto flex flex-col 
-      sm:flex-row w-fit border-none p-1 items-center"
+      onMouseLeave={() => setPosition((pv) => ({ ...pv, opacity: 0 }))}
+      className="relative mx-auto flex flex-col sm:flex-row w-fit border-none p-1 items-center"
     >
       {navigationItems.map((item, index) => (
-        <Tab
-          key={index.toString()}
-          href={item.href}
-          setPositionDesktopOrTable={setPositionDesktopOrTable}
-          isDesktopOrTable={isDesktopOrTable}
-          setPositionMobile={setPositionMobile}
-        >
-          <p
-            className={cn(
-              "font-din-bold text-sm",
-              pathname === item.href && "sm:text-primary"
-            )}
-          >
+        <Tab key={index} href={item.href} onMouseEnter={handleMouseEnter}>
+          <p className={cn("font-din-bold text-sm", pathname === item.href && `sm:text-primary ${!isDesktopOrTablet && "text-white"}` )}>
             {item.title}
           </p>
         </Tab>
       ))}
-      {isDesktopOrTable ? (
-        <CursorDesktopOrTable position={positionDesktopOrTable} />
+      {isDesktopOrTablet ? (
+        <CursorDesktop position={position} />
       ) : (
-        <CursorMobile position={positionMobile} />
+        <CursorMobile position={position} />
       )}
     </ul>
   );
@@ -82,96 +65,45 @@ export const NavBar = () => {
 interface TabProps {
   href: string;
   children: React.ReactNode;
-  setPositionDesktopOrTable: ({
-    left,
-    width,
-    opacity,
-  }: {
-    left: number;
-    width: number;
-    opacity: number;
-  }) => void;
-  setPositionMobile: ({
-    top,
-    width,
-    opacity,
-  }: {
-    top: number;
-    width: number;
-    opacity: number;
-  }) => void;
-  isDesktopOrTable: boolean;
+  onMouseEnter: (ref: HTMLLIElement | null) => void;
 }
 
-const Tab = ({
-  href,
-  children,
-  setPositionDesktopOrTable,
-  setPositionMobile,
-  isDesktopOrTable,
-}: TabProps) => {
+const Tab = ({ href, children, onMouseEnter }: TabProps) => {
   const ref = useRef<HTMLLIElement | null>(null);
   return (
     <li
       ref={ref}
-      onMouseEnter={() => {
-        if (!ref.current) return;
-
-        const { width } = ref.current.getBoundingClientRect();
-
-        if (isDesktopOrTable) {
-          setPositionDesktopOrTable({
-            left: ref.current.offsetLeft,
-            width,
-            opacity: 1,
-          });
-        } else {
-          setPositionMobile({
-            top: ref.current.offsetTop,
-            width,
-            opacity: 1,
-          });
-        }
-      }}
-      className="relative z-10 block cursor-pointer px-3 py-1.5
-                uppercase text-light-100 md:px-5 md:py-3
-                md:text-base"
+      onMouseEnter={() => onMouseEnter(ref.current)}
+      className="relative z-10 block cursor-pointer px-3 py-1.5 uppercase text-light-100 md:px-5 md:py-3 md:text-base"
     >
       <Link href={href}>{children}</Link>
     </li>
   );
 };
 
-interface CursorDesktopOrTableProps {
+interface CursorProps {
   position: {
     left: number;
     width: number;
     opacity: number;
-  };
-}
-
-const CursorDesktopOrTable = ({ position }: CursorDesktopOrTableProps) => {
-  return (
-    <motion.li
-      animate={position}
-      className="absolute z-0 h-1 rounded-full bg-primary translate-y-4"
-    />
-  );
-};
-
-interface CursorMobileProps {
-  position: {
     top: number;
-    width: number;
-    opacity: number;
   };
 }
 
-const CursorMobile = ({ position }: CursorMobileProps) => {
-  return (
-    <motion.li
-      animate={position}
-      className="absolute z-0 h-7 w-full rounded-full bg-primary scale-125"
-    />
-  );
-};
+const CursorDesktop = ({ position }: CursorProps) => (
+  <motion.li
+    animate={{ left: position.left, width: position.width, opacity: position.opacity }}
+    className="absolute z-0 h-1 rounded-full bg-primary translate-y-4"
+  />
+);
+
+const CursorMobile = ({ position }: CursorProps) => (
+  <motion.li
+    animate={{
+      top: position.top, // Use height of first item
+      width: position.width,
+      opacity: 1,
+    }}
+    className="absolute z-0 h-7 rounded-full bg-primary scale-125"
+  />
+);
