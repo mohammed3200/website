4bcbc333a1e762d2e9077b7d836f9eca10a41743
mohammed3200 <wwyuu799@gmail.com>
2024-12-06 20:54:10 +0200

    constants

---
+++ src/constants/icons/index.ts
@@ -0,0 +1,16 @@
+export const socialsIcons = {
+    facebook : "/assets/socials/facebook.svg",
+    X: "/assets/socials/x.svg",
+    Telegram :"/assets/socials/telegram.svg",
+    Whatsapp: "/assets/socials/whatsapp.svg"
+};
+
+export const MainLogo = {
+    Logo: "/assets/icons/logo.svg",
+    LogoFull : "/assets/icons/logo-full.svg",
+    logoEnglishFull : "/assets/icons/logo-english-full.svg",
+};
+
+export const IconsInterface = {
+    Menu: "/assets/icons/menu.svg",   
+}
---
+++ src/constants/images/index.ts
@@ -0,0 +1,9 @@
+export const strategicsImage = {
+    college : "/assets/icons/collegeWithEffect.png",
+    leadership : "/assets/icons/logoWithEffect.png",
+};
+
+export const BackGroundEffect = {
+    GlassBackground: "/assets/images/GlassBack.png",
+    Noise: "/assets/images/noise.webp"
+}
