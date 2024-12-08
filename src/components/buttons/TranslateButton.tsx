0d95bddd4cbdeb5be3e45166938dc5e56959e604
mohammed3200 <wwyuu799@gmail.com>
2024-12-08 10:16:20 +0200

    fix and upgrand translate button

--- src/components/buttons/TranslateButton.tsx
+++ src/components/buttons/TranslateButton.tsx
@@ -5,7 +5,10 @@ import React, { useTransition } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-const locales = ["ar", "en"];
+const locales = [
+  { code: "en", title: "English" },
+  { code: "ar", title: "العربي" },
+];
 
 export const TranslateButton = () => {
   const router = useRouter();
@@ -14,30 +17,31 @@ export const TranslateButton = () => {
 
   const handleChange = (localeCode: string) => {
     startTransition(() => {
-      // Get the current path segments
-      const pathSegments = pathname.split('/').filter(Boolean);
-
-      // Update the first segment with the new locale
+      const pathSegments = pathname.split("/").filter(Boolean);
       if (pathSegments.length > 0) {
-        pathSegments[0] = localeCode; // Update the locale segment
-        const newPath = `/${pathSegments.join('/')}`; // Reconstruct the path
-        router.replace(newPath); // Navigate to the new path
+        pathSegments[0] = localeCode; 
+        const newPath = `/${pathSegments.join("/")}`; 
+        router.replace(newPath); 
       }
     });
   };
 
+  // Find the first language code in the pathname
+  const currentLocale = () => pathname.split("/").filter(Boolean)[0];
+
+
   return (
     <div className="flex flex-row items-center space-x-1">
-      {locales.map((code, index) => (
-        <React.Fragment key={code}>
+      {locales.map((locale, index) => (
+        <React.Fragment key={locale.code}>
           <button
-            onClick={() => handleChange(code)}
+            onClick={() => handleChange(locale.code)}
             className={cn(
               "font-din-bold cursor-pointer",
-              pathname.includes(code) ? "text-primary" : "text-light-100"
+              currentLocale() === locale.code ? "text-primary" : "text-light-100"
             )}
           >
-            {code}
+            {locale.title}
           </button>
           {locales.length - 1 > index && (
             <div className="flex rounded-full h-5 w-[1px] bg-black" />
