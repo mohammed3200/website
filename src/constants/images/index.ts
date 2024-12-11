461e8035ff193924092a8877886ce996b62e42c1
mohammed3200 <wwyuu799@gmail.com>
2024-12-11 17:42:56 +0200

    Illustration for interfacs

--- src/constants/images/index.ts
+++ src/constants/images/index.ts
@@ -9,5 +9,23 @@ export const BackGroundEffect = {
 }
 
 export const DefaultImage = {
-    NewsDefault: "/assets/images/NewsDefault.png",
+    NewsDefault: "/assets/images/News-rafiki.svg",
+};
+
+export const InterfaceImage = {
+    Item1: "/assets/images/item1.svg",
+    Error404 : "/assets/images/404Error.svg",
+    AnalysisBro : "/assets/images/AnalysisBro.svg",
+    AnalysisPana : "/assets/images/AnalysisPana.svg",
+    Office : "/assets/images/Office.svg",
+    Form : "/assets/images/Form.svg",
+    ComputerError : "/assets/images/ComputerError.svg",
+    DataExtraction : "/assets/images/DataExtraction.svg",
+    DataExtraction2 : "/assets/images/DataExtraction2.svg",
+    Innovation : "/assets/images/Innovation.svg",
+    InnovationBro :"/assets/images/Innovation-bro.svg",
+    InnovationRafiki : "/assets/images/Innovation-rafiki.svg",
+    NoData : "/assets/images/NoData.svg",
+    Online :"/assets/images/Online-world-bro.svg",
+    Warning : "/assets/images/Warning.svg",
 }
