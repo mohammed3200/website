3b50eadecbb59000a1e8ae5a04477acaf5178969
mohammed3200 <wwyuu799@gmail.com>
2024-12-20 22:41:11 +0200

    add Distribution center icons

--- src/constants/icons/index.ts
+++ src/constants/icons/index.ts
@@ -13,4 +13,11 @@ export const MainLogo = {
 
 export const IconsInterface = {
     Menu: "/assets/icons/menu.svg",   
+    Upload: "/assets/icons/upload.svg",
+    Email: "/assets/icons/email.svg",
+    Location: "/assets/icons/location.svg",
+    Site: "/assets/icons/site.svg",
+    File: "/assets/icons/file.svg",
+    Trash: "/assets/icons/trash.svg",
+    Send : "/assets/icons/send.svg",
 }
