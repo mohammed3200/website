4e4edcbd439d70b85e9f504780f8823aa3144158
mohammed3200 <wwyuu799@gmail.com>
2024-12-20 22:43:10 +0200

    add Distribution center images

--- src/constants/images/index.ts
+++ src/constants/images/index.ts
@@ -1,31 +1,40 @@
 export const strategicsImage = {
-    college : "/assets/icons/collegeWithEffect.png",
-    leadership : "/assets/icons/logoWithEffect.png",
+  college: "/assets/icons/collegeWithEffect.png",
+  leadership: "/assets/icons/logoWithEffect.png",
 };
 
 export const BackGroundEffect = {
-    GlassBackground: "/assets/images/GlassBack.png",
-    Noise: "/assets/images/noise.webp"
-}
+  GlassBackground: "/assets/images/GlassBack.png",
+  Noise: "/assets/images/noise.webp",
+  CardCurve: "/assets/images/CardCurve.svg",
+  TransparentCircle: "/assets/images/TransparentCircle.svg"
+};
 
 export const DefaultImage = {
-    NewsDefault: "/assets/images/News-rafiki.svg",
+  NewsDefault: "/assets/images/News-rafiki.svg",
 };
 
 export const InterfaceImage = {
-    Item1: "/assets/images/item1.svg",
-    Error404 : "/assets/images/404Error.svg",
-    AnalysisBro : "/assets/images/AnalysisBro.svg",
-    AnalysisPana : "/assets/images/AnalysisPana.svg",
-    Office : "/assets/images/Office.svg",
-    Form : "/assets/images/Form.svg",
-    ComputerError : "/assets/images/ComputerError.svg",
-    DataExtraction : "/assets/images/DataExtraction.svg",
-    DataExtraction2 : "/assets/images/DataExtraction2.svg",
-    Innovation : "/assets/images/Innovation.svg",
-    InnovationBro :"/assets/images/Innovation-bro.svg",
-    InnovationRafiki : "/assets/images/Innovation-rafiki.svg",
-    NoData : "/assets/images/NoData.svg",
-    Online :"/assets/images/Online-world-bro.svg",
-    Warning : "/assets/images/Warning.svg",
+  Error404: "/assets/images/404Error.svg",
+  AnalysisBro: "/assets/images/AnalysisBro.svg",
+  AnalysisPana: "/assets/images/AnalysisPana.svg",
+  Office: "/assets/images/Office.svg",
+  Form: "/assets/images/Form.svg",
+  ComputerError: "/assets/images/ComputerError.svg",
+  DataExtraction: "/assets/images/DataExtraction.svg",
+  DataExtraction2: "/assets/images/DataExtraction2.svg",
+  Innovation: "/assets/images/Innovation.svg",
+  InnovationBro: "/assets/images/Innovation-bro.svg",
+  InnovationRafiki: "/assets/images/Innovation-rafiki.svg",
+  NoData: "/assets/images/NoData.svg",
+  Online: "/assets/images/Online-world-bro.svg",
+  Warning: "/assets/images/Warning.svg",
+};
+
+export const CorporateCompanies = {
+  AfterPay: "/assets/corporateLogos/afterpay.svg",
+  Amplitude : "/assets/corporateLogos/amplitude.svg",
+  Drips: "/assets/corporateLogos/drips.svg",
+  Maze: "/assets/corporateLogos/maze.svg",
+  Sonos : "/assets/corporateLogos/sonos.svg",
 }
