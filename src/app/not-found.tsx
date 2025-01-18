79e0b447bcae7d5f88ffd756360bce87d4d3e1dd
mohammed3200 <wwyuu799@gmail.com>
2025-01-18 21:36:19 +0200

    create not found screen

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,64 @@
 "use client";
 
-// TODO: not found screen
 import Link from "next/link";
-import { AlertTriangle } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { InterfaceImage } from "@/constants";
+import useLanguage from "@/hooks/uselanguage";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const NotFound = () => {
+  const { isArabic, lang } = useLanguage();
   return (
-    <div className="h-screen flex flex-col gap-y-4 items-center justify-center">
-      <AlertTriangle className="size-6" />
-      <p className="text-sm text-muted-foreground">Something went wrong</p>
-      <Button variant="secondary" size="sm">
-        <Link href={"/"}>Back to home</Link>
-      </Button>
-    </div>
+    <html>
+      <body>
+        <div>
+          <BackgroundBeams className="-z-10" />
+          <main className="flex h-screen">
+            <section className="flex h-full flex-1 flex-col">
+              <div
+                className="flex flex-1 items-center justify-center flex-col gap-2"
+                dir={isArabic ? "rtl" : "ltr"}
+              >
+                <Image
+                  src={InterfaceImage.Error404}
+                  alt="not found"
+                  width={300}
+                  height={300}
+                  sizes="(max-width: 640px) 100vw, (min-width: 641px) 50vw" // Responsive sizes for mobile and desktop
+                  className="object-cover h-auto"
+                />
+                <h2 className="font-din-bold h6 md:h5 ">
+                  {isArabic ? "لم يتم العثور على الصفحة" : "Page not found"}
+                </h2>
+                <div className="font-din-regular body-2 max-lg:max-w-sm">
+                  {isArabic ? (
+                    <p className="">
+                      لم نتمكن من العثور على الصفحة التي تبحث عنها. يرجى التحقق
+                      من عنوان URL أو العودة إلى{" "}
+                      <Link href={`/${lang}`}>
+                        <span className="text-primary cursor-pointer hover:underline hover:decoration-4 hover:underline-offset-[6px]">
+                          الصفحة الرئيسية
+                        </span>
+                      </Link>
+                    </p>
+                  ) : (
+                    <p className="">
+                      We couldn&apos;t find the page you were looking for.
+                      Please check the URL or return to{" "}
+                      <Link href={`/${lang}`}>
+                        <span className="text-primary cursor-pointer hover:underline hover:decoration-4 hover:underline-offset-[6px]">
+                          Home Page
+                        </span>
+                      </Link>
+                    </p>
+                  )}
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      </body>
+    </html>
   );
 };
 
