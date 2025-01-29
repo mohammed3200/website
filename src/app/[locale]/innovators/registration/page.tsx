251c031c3ba48cca9880f2c776e2d8bbc2d729cb
mohammed3200 <wwyuu799@gmail.com>
2025-01-29 22:27:51 +0200

    add Registration Innovators Form

--- src/app/[locale]/innovators/registration/page.tsx
+++ src/app/[locale]/innovators/registration/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { InnovatorsRegistrationForm } from "@/features/innovators/components";
 
 const Registration = () => {
-  return (<div className="overflow-hidden">
-    
-  </div>);
+  return (
+    <div className="overflow-hidden">
+      <InnovatorsRegistrationForm />
+    </div>
+  );
 };
 
 export default Registration;
