37215332782d0885c1279cd0776d9354ca629ae2
mohammed3200 <wwyuu799@gmail.com>
2025-02-07 20:08:48 +0200

    Create a route API for innovators and creators

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,13 +2,15 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import collaborator from "@/features/collaborators/server/route";
+import innovators from "@/features/innovators/server/route";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
-    .route("/collaborator", collaborator)
+  .route("/collaborator", collaborator)
+  .route("/innovators", innovators);
 
 export const GET = handle(app);
 export const POST = handle(app);
