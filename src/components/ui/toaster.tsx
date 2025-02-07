45f8947ca20c50f96f130fe0549698aba86c3b95
mohammed3200 <wwyuu799@gmail.com>
2025-02-07 20:16:52 +0200

    Add Arabic font family  to the component toast, and add success and failure icons to the component toaster

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -9,17 +9,22 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { CircleCheck, CircleX } from "lucide-react"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, success, error, action, ...props }) {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+              <div className="flex flex-row gap-4">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {success && <CircleCheck color="#00ff80" />}
+                {error && <CircleX color="#ff4233" />}
+              </div>
               {description && (
                 <ToastDescription>{description}</ToastDescription>
               )}
