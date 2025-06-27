1a0ea8d5e2f8efd9abdb5b91bd0312beb5385163
faraj-elahjal <wwyuu799@gmail.com>
2025-06-27 23:13:51 +0200

    refactor: enhance code formatting and organization in constants and schemas

--- src/features/innovators/constants.ts
+++ src/features/innovators/constants.ts
@@ -2,23 +2,23 @@ import { StageDevelopment } from "./types"
 
 export const StagesDevelopment = {
     [StageDevelopment.STAGE] : {
-        en: "Idea Stage",
-        ar: "مرحلة الفكرة",
-    },
+    en: "Idea Stage",
+    ar: "مرحلة الفكرة",
+  },
     [StageDevelopment.PROTOTYPE] : {
         en: "prototype",
-        ar: "النموذج الأولي"
-    },
+    ar: "النموذج الأولي"
+  },
     [StageDevelopment.DEVELOPMENT] : {
         en: "Under construction",
-        ar: "قيد الانشاء",
-    },
+    ar: "قيد الانشاء",
+  },
     [StageDevelopment.TESTING] : {
         en: "Evaluation phase",
-        ar: "مرحلة التقييم",
-    },
+    ar: "مرحلة التقييم",
+  },
     [StageDevelopment.RELEASED] : {
         en: "Ready to launch",
-        ar: "جاهز للاطلاق",
-    }
+    ar: "جاهز للاطلاق",
+  }
 }
