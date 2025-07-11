d6d73060836a7e117e1f2a502143323b70b259d1
mohammed3200 <wwyuu799@gmail.com>
2025-07-11 19:45:37 +0200

    feat: Add success and error  forms components to display a message to users.

---
+++ src/features/auth/components/form-error.tsx
@@ -0,0 +1,16 @@
+import { TriangleAlert } from 'lucide-react';
+
+interface FormErrorProps {
+    message?: string;
+};
+
+export const FormError = ({ message }: FormErrorProps) => {
+    if (!message) return null;
+    
+    return (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive">
+            <TriangleAlert size={16} />
+            <span>{message}</span>
+        </div>
+    );
+};
