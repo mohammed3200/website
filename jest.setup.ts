71489e0bc1cee14b4d08bc2e3743336d66fbb830
Cit2024 <expo@cit.edu.ly>
2026-04-18 19:14:58 +0200

    refactor: remove Jest testing framework and associated dependencies

--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom/jest-globals';
-import { afterEach } from '@jest/globals';
-import { cleanup } from '@testing-library/react';
-
-afterEach(() => {
-  cleanup();
-});
