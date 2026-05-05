1c60ad85ae5f337eafe5d203e5f64a416e8d3038
mohammed3200 <wwyuu799@gmail.com>
2026-05-05 19:30:06 +0200

    chore: clean up icons and remove unused corporate logos
    - Replace college.png with college.svg
    - Remove unused corporate logo SVGs (afterpay, amplitude, drips, maze, sonos)
    - Fix whitespace formatting in icons/index.ts

--- src/constants/icons/index.ts
+++ src/constants/icons/index.ts
@@ -1,34 +1,34 @@
 export const MainLogo = {
     Logo: "/assets/icons/logo.svg",
-    LogoCollege: "/assets/icons/college.png",
+    LogoCollege: "/assets/icons/college.svg",
 
     // Leadership Center Logos Large Size
-    CenterLogoLarge : "/assets/icons/Leadership-Center-Logo-Arabic-For-the-big-screen.svg",
-    CenterLogoLargeEnglish : "/assets/icons/Leadership-Center-Logo-English-For-the-big-screen.svg",
+    CenterLogoLarge: "/assets/icons/Leadership-Center-Logo-Arabic-For-the-big-screen.svg",
+    CenterLogoLargeEnglish: "/assets/icons/Leadership-Center-Logo-English-For-the-big-screen.svg",
 
     // Leadership Center Logo Small Size
-    CenterLogoSmall : "/assets/icons/Leadership-Center-Logo-Arabic-For-the-small-screen.svg",
+    CenterLogoSmall: "/assets/icons/Leadership-Center-Logo-Arabic-For-the-small-screen.svg",
     CenterLogoSmallEnglish: "/assets/icons/Leadership-Center-Logo-English-For-the-small-screen.svg",
 
     // College Logos Large Size
-    CollegeLogoLarge : "/assets/icons/Industrial-Technology-College-Logo-Arabic-For-the-big-screen.svg",
-    CollegeLogoLargeEnglish : "/assets/icons/Industrial-Technology-College-Logo-English-For-the-big-screen.svg",
+    CollegeLogoLarge: "/assets/icons/Industrial-Technology-College-Logo-Arabic-For-the-big-screen.svg",
+    CollegeLogoLargeEnglish: "/assets/icons/Industrial-Technology-College-Logo-English-For-the-big-screen.svg",
 
     // College Logos Small Size
-    CollegeLogoSmall : "/assets/icons/Industrial-Technology-College-Logo-Arabic-For-the-small-screen.svg",
-    CollegeLogoSmallEnglish : "/assets/icons/Industrial-Technology-College-Logo-English-For-the-small-screen.svg",
+    CollegeLogoSmall: "/assets/icons/Industrial-Technology-College-Logo-Arabic-For-the-small-screen.svg",
+    CollegeLogoSmallEnglish: "/assets/icons/Industrial-Technology-College-Logo-English-For-the-small-screen.svg",
 
 };
 
 export const IconsInterface = {
-    Menu: "/assets/icons/menu.svg",   
+    Menu: "/assets/icons/menu.svg",
     Upload: "/assets/icons/upload.svg",
     Email: "/assets/icons/email.svg",
     Location: "/assets/icons/location.svg",
     Site: "/assets/icons/site.svg",
     File: "/assets/icons/file.svg",
     Trash: "/assets/icons/trash.svg",
-    Send : "/assets/icons/send.svg",
-    User : "/assets/icons/user.svg",
-    Text : "/assets/icons/text.svg",
+    Send: "/assets/icons/send.svg",
+    User: "/assets/icons/user.svg",
+    Text: "/assets/icons/text.svg",
 }
